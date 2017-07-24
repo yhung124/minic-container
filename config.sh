06efbf0c5e0cca44d5d67c582477abd8efbc86d9
Raymond <yhung124@gmail.com>
2017-07-24 05:54:40 +0000

    Remove unused variables

--- config.sh
+++ config.sh
@@ -1,6 +1,3 @@
-MINIC_BUILDER_IMAGE="cdxvirt/minic"
-MINIC_BUILDER_IMAGE_TAG="latest"
-
 # Modify the path of registry here if you need
 : ${REGISTRY:="cr.cdxvirt/cdxvirt-mini"}
 : ${REL_TAG:="latest"}
